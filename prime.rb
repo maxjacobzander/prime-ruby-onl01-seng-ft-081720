fe0623763caf788acbbb8cb268ca84d195654839
Max Zander <maxjacobzander@gmail.com>
2020-08-21 19:37:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    false
+  elsif prime_numbers = (2..n-1).to_a
+    prime_numbers.none? do |numbers_checked|
+      n % numbers_checked == 0
+      true
+    end
+  end
+end
