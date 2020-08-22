07d5014ec4b54078511a99ebe779e5ca8c196b53
maxjacobzander <maxjacobzander@gmail.com>
2020-08-22 11:31:31 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 def prime?(n)
   if n < 2
     false
-  elsif prime_numbers = (2..n-1).to_a
-    prime_numbers.none? do |numbers_checked|
+  elsif (2..n-1).none? do |numbers_checked|
       n % numbers_checked == 0
-      true
     end
+    true
+  else
+    false
   end
 end
